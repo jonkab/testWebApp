94ee645767a38e9e0f68bdad4bdf144d6d6e07b0
jonkab <jonathan.kabrit@gmail.com>
2016-10-17 18:20:56 +0200

    start project DIAMS

--- app/scripts/modules/jquery.creatList.js
+++ app/scripts/modules/jquery.creatList.js
@@ -66,6 +66,19 @@ var jQuery = require('jquery/dist/jquery.min');
 
                 }
             }
+/*
+            $.getJSON( "../mock/languages.json", function( data ) {
+                var items = [];
+                $.each( data, function( key, val ) {
+                    items.push( "<li id='" + key + "'>" + val + "</li>" );
+                });
+
+                $( "<ul/>", {
+                    "class": "my-new-list",
+                    html: items.join( "" )
+                }).appendTo( "body" );
+            });*/
+
         }
 
 
